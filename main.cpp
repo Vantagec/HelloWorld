434a1e886f8d3b36c1d8234acb088d4c51b100c1
Vantagec <163841699+Vantagec@users.noreply.github.com>
2024-03-19 16:32:57 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+//cccccccccc
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        int build_number = std::atoi(argv[1]);
+        std::cout << "build " << build_number << std::endl;
+    }
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
