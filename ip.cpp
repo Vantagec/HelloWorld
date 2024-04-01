eda56fda762b4af204281c5230c670f198bde245
Vantagec <163841699+Vantagec@users.noreply.github.com>
2024-04-01 12:07:39 +0300

    Update ip.cpp

--- ip.cpp
+++ ip.cpp
@@ -2,9 +2,11 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <sstream>
+#include <array>
 
 struct IPAddress {
-    std::string address;
+    std::array<int, 4> address;
     int first_byte;
     int second_byte;
     int third_byte;
@@ -12,27 +14,21 @@ struct IPAddress {
 };
 
 bool compareIPAddresses(const IPAddress& ip1, const IPAddress& ip2) {
-    if (ip1.first_byte != ip2.first_byte) {
-        return ip1.first_byte > ip2.first_byte;
-    } else if (ip1.second_byte != ip2.second_byte) {
-        return ip1.second_byte > ip2.second_byte;
-    } else if (ip1.third_byte != ip2.third_byte) {
-        return ip1.third_byte > ip2.third_byte;
-    } else {
-        return ip1.fourth_byte > ip2.fourth_byte;
-    }
+    return ip1.address < ip2.address;
 }
 
 int main() {
     std::vector<IPAddress> ipAddresses;
-    
+
     std::string line;
     while (std::getline(std::cin, line)) {
         size_t pos = line.find('\t');
         if (pos != std::string::npos) {
             IPAddress ip;
-            ip.address = line.substr(0, pos);
-            sscanf(ip.address.c_str(), "%d.%d.%d.%d", &ip.first_byte, &ip.second_byte, &ip.third_byte, &ip.fourth_byte);
+            std::istringstream iss(line.substr(0, pos));
+            char dot;
+            iss >> ip.first_byte >> dot >> ip.second_byte >> dot >> ip.third_byte >> dot >> ip.fourth_byte;
+            ip.address = {ip.first_byte, ip.second_byte, ip.third_byte, ip.fourth_byte};
             ipAddresses.push_back(ip);
         }
     }
@@ -40,14 +36,14 @@ int main() {
     std::sort(ipAddresses.begin(), ipAddresses.end(), compareIPAddresses);
 
     for (const auto& ip : ipAddresses) {
-        std::cout << ip.address << std::endl;
+        std::cout << ip.first_byte << '.' << ip.second_byte << '.' << ip.third_byte << '.' << ip.fourth_byte << std::endl;
     }
 
     std::cout << std::endl;
 
     for (const auto& ip : ipAddresses) {
         if (ip.first_byte == 1) {
-            std::cout << ip.address << std::endl;
+            std::cout << ip.first_byte << '.' << ip.second_byte << '.' << ip.third_byte << '.' << ip.fourth_byte << std::endl;
         }
     }
 
@@ -55,18 +51,17 @@ int main() {
 
     for (const auto& ip : ipAddresses) {
         if (ip.first_byte == 46 && ip.second_byte == 70) {
-            std::cout << ip.address << std::endl;
+            std::cout << ip.first_byte << '.' << ip.second_byte << '.' << ip.third_byte << '.' << ip.fourth_byte << std::endl;
         }
     }
 
     std::cout << std::endl;
 
     for (const auto& ip : ipAddresses) {
-        if (ip.address.find(".46.") != std::string::npos) {
-            std::cout << ip.address << std::endl;
+        if (ip.address[0] == 46) {
+            std::cout << ip.first_byte << '.' << ip.second_byte << '.' << ip.third_byte << '.' << ip.fourth_byte << std::endl;
         }
     }
 
     return 0;
 }
-
