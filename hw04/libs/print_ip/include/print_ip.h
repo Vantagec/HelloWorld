02702181ce139b44128e463ca0cece3ebfef88e7
Vantagec <163841699+Vantagec@users.noreply.github.com>
2024-05-21 00:45:26 +0300

    Create print_ip.h

---
+++ hw04/libs/print_ip/include/print_ip.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "custom_traits.h"
+
+
+template <typename T>
+std::ostream &print_ip(T &&value, std::ostream &outStream = std::cout)
+{
+    return print_ip_impl(std::forward<T>(value), outStream);
+}
+ 
